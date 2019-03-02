7db932401052899e25f1d30c9e1f9148ed6d1be8
Zoe Bai <s.xue.bai@gmail.com>
2019-03-02 05:42:49 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,4 +3,49 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
+  attr_accessor :name, :grade
+  attr_reader :id
+  @@all = []  
+  
+  def initialize(id = nil, name, grade)
+    @id = id
+    @name = name
+    @grade = grade
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.create_table
+    DB[:conn].execute(
+        "CREATE TABLE students(
+          id INTEGER PRIMARY KEY,
+          name TEXT,
+          grade INTEGER
+          );"
+      )
+  end
+  
+  def self.drop_table
+    DB[:conn].execute(
+        "DROP TABLE students;"
+      )
+  end
+  
+  def save
+     DB[:conn].execute(
+        "INSERT INTO students (name, grade) VALUES (?, ?)",
+        [self.name, self.grade]
+       )
+     @id = DB[:conn].last_insert_row_id 
+     
+  end
+  
+  def self.create(name:,grade:)
+    student = Student.new(name, grade) # @name = name  name is the value you pass in
+    student.save
+    student
+  end
 end
