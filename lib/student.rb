12bec9451087c40f0a2031b9736de8a4bc348dfe
Zoe Bai <s.xue.bai@gmail.com>
2019-03-02 05:37:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -43,7 +43,7 @@ class Student
      
   end
   
-  def self.create({name:, grade:})
+  def self.create(name:, grade:)
     student = Student.new({name: self.name,  grade: self.grade})
     student.save
     student
